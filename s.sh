9b160448d7c175e9d4db7ec737f99fd8f3f1e1f0
xAezteru_ <veraezter@gmail.com>
2025-05-03 12:34:13 +0700

    null

--- s.sh
+++ s.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
-pw="Miner-$(( (RANDOM << 15 | RANDOM) % 1000000 + 1 ))"
-sed -i "s/\"pass\": \".*\"/\"pass\": \"$pw\"/" config.json
-chmod +x .xm
-clear
-./.xm --config config.json
+usage() {
+  echo "usage: bash $0 -b <t|f>"
+  exit 1
+}
+
+while getopts "b:" opt; do
+  case $opt in
+    b)
+      ip=$(curl -s https://api.ipify.org)
+      [ -z "$ip" ] && echo "Gagal mengambil IP publik" && exit 1
+
+      pw="Miner-$ip"
+      sed -i "s/\"pass\": \".*\"/\"pass\": \"$pw\"/" config.json
+
+      if [ "$OPTARG" == "t" ]; then
+        sed -i 's/"background": *[^,]*/"background": true/' config.json
+      elif [ "$OPTARG" == "f" ]; then
+        sed -i 's/"background": *[^,]*/"background": false/' config.json
+      else
+        usage
+      fi
+
+      chmod +x .xm
+      clear
+      echo "Your MinerName \"$pw\" and you"
+      ./.xm
+      exit 0
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+usage
